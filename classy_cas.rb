0c0900c878262320ea75b134ecbba68ec3d3a803
Tim Case <tim@powerupdev.com>
2010-11-05 20:55:50 -0700

    fixing bad url config

--- classy_cas.rb
+++ classy_cas.rb
@@ -22,7 +22,7 @@ configure do
     
     FIRST_SITE_URL = 'http://quiet-samurai-41.heroku.com'
     SECOND_SITE_URL = 'http://cold-river-72.heroku.com'
-    USER_STORE_URL = 'http://:stormy-river-84.heroku.com'
+    USER_STORE_URL = 'http://stormy-river-84.heroku.com'
   else
     FIRST_SITE_URL = 'http://redrum.local'
     SECOND_SITE_URL = 'http://greenie.local'
