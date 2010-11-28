69821e1bf48e19ab93010396ec743282183e98cf
Tim Case <tim@poweupdev.com>
2010-11-28 14:50:35 -0200

    removing app live call for bug fixing

--- lib/user_store.rb
+++ lib/user_store.rb
@@ -5,10 +5,11 @@ class UserStore
   def self.authenticate(username, password)
     app_config = YAML.load_file("../config/classy_cas.yml")[ENV['RACK_ENV']]
     begin
-      RestClient.post "#{app_config['user_store_url']}/users/sign_in.xml", 
-                                :user => {:email => username, 
-                                :password => password}, 
-                                :content_type => :xml
+      true
+      # RestClient.post "#{app_config['user_store_url']}/users/sign_in.xml", 
+      #                           :user => {:email => username, 
+      #                           :password => password}, 
+      #                           :content_type => :xml
     rescue RestClient::Request::Unauthorized
       return false
     rescue RestClient::Found
